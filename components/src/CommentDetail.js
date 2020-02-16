37ead248d6ac557c4f4e31d916bfa081ecbbc448
TristanR99 <48964179+TristanR99@users.noreply.github.com>
2020-02-16 13:14:50 -0500

    Added approval card

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,20 +1,19 @@
 import React from "react";
-import faker from "faker";
 
-const CommentDetail = () => {
+const CommentDetail = ({ author, time, content, avatar }) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
-          {faker.name.firstName(1)}
+          {author}
         </a>
         <div className="metadata">
-          <span className="date">{faker.date.weekday(true, true)}</span>
+          <span className="date">{time}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">{content}</div>
       </div>
     </div>
   );
--- components/src/index.js
+++ components/src/index.js
@@ -2,12 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <ApprovalCard />
+      <CommentDetail
+        author={faker.name.firstName(1)}
+        time="Today at 4:00PM"
+        content={faker.lorem.sentences(1)}
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author={faker.name.firstName(0)}
+        time="Yesterday at 2:30AM"
+        content={faker.lorem.sentences(1)}
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author={faker.name.firstName(1)}
+        time="Monday at 6:45PM"
+        content={faker.lorem.sentences(1)}
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
